358224dd388afd2bdb34f54bf8dab4264e7c8acc
Pragnesh-Papaniya <pragneshpapaniya007@gmail.com>
2024-05-25 20:11:51 +0530

    simple blinky

---
+++ led_blink/Objects/main.d
@@ -0,0 +1,10 @@
+./objects/main.o: main.c \
+  C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
+  RTE\_Target_1\RTE_Components.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\6.0.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\6.0.0\CMSIS\Core\Include\m-profile\cmsis_armclang_m.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\arm_acle.h \
+  C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h
