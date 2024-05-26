dfffffab7d9a61ba9f9ec042991f4cbae55e2505
Pragnesh-Papaniya <pragneshpapaniya007@gmail.com>
2024-05-26 09:02:29 +0530

    wrote GPIO driver
    used GPIO driver instead of header file

--- learning_code/push_button/main.c
+++ learning_code/push_button/main.c
@@ -1,27 +1,22 @@
-#include "stm32f10x.h"                  // Device header
-
+#include "GPIO.h"
 void Delays(int time);
 
 int main(void)
 {
-	RCC->APB2ENR |= 4; //// Enabling PORT A
-	RCC->APB2ENR |= 0x10; /// Enabling PORT C
-	GPIOA->CRL &= 0xFFFFFFF0; /// Reset the Port A PIN 0
-	GPIOA->CRL |= 0x8; /// Set port A pin 0 as Input push pull
-	
-	GPIOC->CRH &= 0xFF0FFFFF; /// Reset the pin 13
-	GPIOC->CRH |= 0x00300000; /// Set PIN 13 as output
+	init_GP(PA,0,IN,I_PP);
+	init_GP(PC,13,OUT50, O_GP_PP);
 	
 	while(1)
 	{
-		if(GPIOA->IDR & 0x00000001) /// Checking status of PIN ! portA 
+		if(R_GP(PA,0)==1) /// Checking status of PIN ! portA 
 		{
-			GPIOC->ODR ^= 0x2000; /// Toggle the PIN state
+			Delays(10);
+			toggle_GP(PC,13); /// Toggle the PIN state
 			Delays(10);
 		}
 		else
 		{
-			GPIOC->ODR = 0x2000; /// Set the PIN 13 port C high
+			W_GP(PC,13,1); /// Set the PIN 13 port C high
 		}
 	}
 	
